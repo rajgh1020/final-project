9e0f6d9bd50466bf4c7f5a62e71547aafe047797
Raj Ghimire <rajghimre1020@gmail.com>
2025-06-02 12:18:03 +0900

    created gamestates object

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const gameState = {
+  playerName: "",
+  playerClass: "",
+  hp: 5,       // Match your hearts count
+  spirit: 3,   // Match your spirit icons
+  inventory: [],
+  currentScene: "intro",
+};
